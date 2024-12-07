5942040c2684fff5ca3a1e01ec76c16267331cc7
271-noor <157873714+271-noor@users.noreply.github.com>
2024-12-07 14:03:16 +0530

    create a home page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,48 @@
-import { Tabs } from 'expo-router';
+// import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
+// import { HapticTab } from '@/components/HapticTab';
+// import { IconSymbol } from '@/components/ui/IconSymbol';
+// import TabBarBackground from '@/components/ui/TabBarBackground';
+// import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Image, StyleSheet, View, Text } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+<View style={styles.container}>
+  
+  {/* Today's Tasks */}
+  <View style={styles.tasksWrapper}>
+    <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+    <View style={styles.items}>
+      {/* This is where the tasks will go! */}
+    </View>
+  </View>
+       
+      </View>
+
   );
+
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: 'bold'
+    },
+    items: {},
+  
+});
